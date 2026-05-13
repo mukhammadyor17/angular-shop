2920a483971f9bd92ce92213c5cf18d3d1a7e003
dior1726 <muxammad_909@mail.ru>
2026-05-13 13:18:12 +0500

    feat: create main layout, home page and general page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './components/layouts/main-layout/main-layout';
+import { AuthLayout } from './components/layouts/auth-layout/auth-layout';
+import { HomePage } from './pages/home-page/home-page';
+import { LoginPage } from './pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        title: 'Home Page',
+        component: HomePage,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '',
+        title: 'Login Page',
+        component: LoginPage,
+      },
+    ],
+  },
+];
