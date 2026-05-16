5e9958444576ddd4c8885e0ac9394b93a476fa42
dior1726 <muxammad_909@mail.ru>
2026-05-16 16:09:30 +0500

    feat: create baseTitle and baseCard components

---
+++ src/app/components/base/base-title/base-title.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-title',
+  imports: [],
+  templateUrl: './base-title.html',
+  styleUrl: './base-title.scss',
+})
+export class BaseTitle {}
