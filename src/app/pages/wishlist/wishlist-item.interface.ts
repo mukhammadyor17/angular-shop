75cf5915d3f8334e16aa5da5469e86198615230b
dzinakor <dzinakorshunava@gmail.com>
2026-05-20 00:01:22 +0400

    feat: implement wishlist service logic and wishlist item interface

---
+++ src/app/pages/wishlist/wishlist-item.interface.ts
@@ -0,0 +1,6 @@
+export interface WishlistItem {
+    id: string;
+    name: string;
+    emage: string;
+    price: number;
+}
--- src/app/pages/wishlist/wishlist.ts
+++ src/app/pages/wishlist/wishlist.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-wishlist',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.scss',
 })
 export class Wishlist {
-
 }
