20c054e30228d223b42b7f3cfe1ad24b47771586
dzinakor <dzinakorshunava@gmail.com>
2026-05-20 18:55:43 +0400

    feat: add wishlist core logic

--- src/app/pages/wishlist/wishlist.ts
+++ src/app/pages/wishlist/wishlist.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { WishlistService } from '../../services/wishlist.service';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.scss',
 })
 export class Wishlist {
+  private wishlistService = inject(WishlistService);
+
+  wishlistItems = this.wishlistService.wishlist;
+
+  totalItems = computed(() => this.wishlistItems().length);
+
+  removeItem(id: string): void {
+    this.wishlistService.removeFromWishLIst(id);
+  }
+
+  clearWishlist(): void {
+    this.wishlistService.clearWishList();
+  }
 }
