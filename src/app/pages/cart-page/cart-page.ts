9cc7b25648053d641269b2de0434ea113c956dc9
Askhat Tassybayev <a.tassybayev@gmail.com>
2026-06-03 20:35:22 +0500

    feat: add development diary entry from 2026-06-03

--- src/app/pages/cart-page/cart-page.ts
+++ src/app/pages/cart-page/cart-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart-page',
+  standalone: true,
   imports: [],
   templateUrl: './cart-page.html',
   styleUrl: './cart-page.scss',
--- src/app/pages/detailed-product-page/detailed-product-page.ts
+++ src/app/pages/detailed-product-page/detailed-product-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-detailed-product-page',
+  standalone: true,
   imports: [],
   templateUrl: './detailed-product-page.html',
   styleUrl: './detailed-product-page.scss',
