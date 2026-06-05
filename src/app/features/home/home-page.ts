b2be0cf224ccd24fde3d25b96afe6cfd87f39ade
dior1726 <muxammad_909@mail.ru>
2026-06-05 15:13:39 +0500

    change: base-title component to style class

--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { BaseTitle } from '../../shared/ui/base-title/base-title';
 
 @Component({
   selector: 'app-home-page',
-  imports: [BaseTitle],
+  imports: [],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
