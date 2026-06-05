617cb2a373914a95d66eae2a9d2768709b9916b7
dior1726 <muxammad_909@mail.ru>
2026-06-05 16:24:02 +0500

    feat: create login page ui

--- src/app/features/auth/login-page/login-page.ts
+++ src/app/features/auth/login-page/login-page.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  submitted = false;
+
+  get email() {
+    return this.form.controls.email;
+  }
+  get password() {
+    return this.form.controls.password;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
