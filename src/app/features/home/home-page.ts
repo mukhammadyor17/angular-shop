2979a875912de6704b10b968347321fe1cd0015a
dior1726 <muxammad_909@mail.ru>
2026-06-05 17:31:47 +0500

    feat: add home page sections

--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { ProductCard } from '../../shared/ui/product-card/product-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+  product = {
+    name: 'Gradient Graphic T-shirt',
+    imageUrl: 'images/gradient-graphic-tshirt.png',
+    rating: 3.5,
+    price: 145,
+    oldPrice: null,
+    discount: null,
+  };
+}
