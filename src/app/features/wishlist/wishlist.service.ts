543cd26d1e0b3c6d838f369e6e7ed5c256de07ff
dzinakor <dzinakorshunava@gmail.com>
2026-07-17 22:29:10 +0400

    feat: add wishlist tab and update guard for only auth users for wishlist

--- src/app/features/wishlist/wishlist.service.ts
+++ src/app/features/wishlist/wishlist.service.ts
@@ -1,29 +1,37 @@
-import { computed, effect, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { WishlistItem } from './wishlist-item.interface';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishlistService {
   private storageKey = 'wishlist';
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
   wishlist = signal<WishlistItem[]>(this.loadWishList());
 
-  totalItems = computed(() => this.wishlist().length); 
+  totalItems = computed(() => this.wishlist().length);
 
   constructor() {
-    /* automatically synchronize state with localStorage */
     effect(() => {
-      localStorage.setItem(this.storageKey, 
-      JSON.stringify(this.wishlist()))
+      localStorage.setItem(this.storageKey, JSON.stringify(this.wishlist()));
     });
-  };
+  }
 
-  addToWishList(product: WishlistItem): void {
-    /* will prevent duplicates */
+  addToWishList(product: WishlistItem): boolean {
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate(['/login'], { queryParams: { redirect: this.router.url } });
+      return false;
+    }
+
     const exist = this.wishlist().some((item) => item.id === product.id);
-    if (exist) return;
+    if (exist) return true;
+
     this.wishlist.update((items) => [...items, product]);
+    return true;
   }
 
   removeFromWishLIst(productId: string): void {
